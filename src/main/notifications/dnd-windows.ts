df37564af0cd76e5774c3970745739ee7fac6849
locflute@gmail.com <locflute@gmail.com>
2025-10-09 16:24:45 +0700

    Update for windows

--- src/main/notifications/dnd-windows.ts
+++ src/main/notifications/dnd-windows.ts
@@ -1,7 +1,17 @@
 // Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {getFocusAssist, isPriority} from 'windows-focus-assist';
+let getFocusAssist: undefined | (() => {value: number});
+let isPriority: undefined | ((appUserModelId: string) => {value: boolean});
+
+try {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const wfa = require('windows-focus-assist');
+    getFocusAssist = wfa.getFocusAssist;
+    isPriority = wfa.isPriority;
+} catch (e) {
+    // Module not available or failed to load; fall back to default behavior.
+}
 
 /**
     -2: NOT_SUPPORTED,
@@ -15,11 +25,20 @@ function getWindowsDoNotDisturb() {
         return false;
     }
 
+    if (!getFocusAssist) {
+        // If we can't read focus assist state, assume DND is off to avoid blocking notifications.
+        return false;
+    }
+
     const focusAssistValue = getFocusAssist().value;
     switch (focusAssistValue) {
     case 2:
         return true;
     case 1:
+        // If isPriority checker is unavailable, treat as not priority to avoid suppressing unnecessarily
+        if (!isPriority) {
+            return false;
+        }
         return !(isPriority('Mattermost.Desktop').value);
     default:
         return false;
